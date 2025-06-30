5946461a025e224e0c23b447b005de6c828378fd
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-06-30 17:45:11 +0200

    Add models to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Tag, Task
+
+
+@admin.register(Task)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("content", "deadline", "is_done")
+    search_fields = ("content",)
+    list_filter = ("tags",)
+
+
+admin.site.register(Tag)
