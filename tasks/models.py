f7024d7e99eb17971e092cc9277e47ce90ae0925
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-06-30 16:38:12 +0200

    Update Task model ordering

--- tasks/models.py
+++ tasks/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
-    content = models.TextField()
-    date = models.DateTimeField()
-    is_done = models.BooleanField()
+    content = models.CharField(max_length=63)
+    date = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
     deadline = models.DateTimeField(null=True, blank=True)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
     class Meta:
-        ordering = ["-is_done", "date"]
+        ordering = ["is_done", "-date"]
